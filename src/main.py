37046442e87446a56b31bc6a9f9fa9e7f5257cc8
luisverges <luisverges@gmail.com>
2019-01-13 21:35:49 +0100

    Testing

--- src/main.py
+++ src/main.py
@@ -2,55 +2,62 @@ import multiprocessing
 from sendmail import sendmail
 from generatelights import showimage, ingestvideo, showvideo
 import os
+import schedule
 
 def getcredentials(file):
     with open(file, 'r') as file:
         username = file.readline()[:-1]
         password=file.readline()
     return [username, password]
+def job():
+    if os.getcwd()[-3:]!='src':
+        os.chdir(os.path.join(os.getcwd(),'src'))
 
-if os.getcwd()[-3:]!='src':
-    os.chdir(os.path.join(os.getcwd(),'src'))
+    to_list=['luisverges@gmail.com','perseofliesagain@gmail.com']
+    output_file=os.path.join('output','mail_status.txt')
 
-to_list=['luisverges@gmail.com','perseofliesagain@gmail.com']
-output_file=os.path.join('output','mail_status.txt')
-
-#Obtain current day
-with open('day.txt', 'r') as file:
-    lines = file.readlines()
-    day = lines[0][:-1]
-day='Day 7' ############
-html_file=open(os.path.join('mails',day+'.html'), 'r', encoding='utf-8')
-html=html_file.read()
-html_file.close()
-
-if day=='Day 0' or day=='Termination':
-    credentials = getcredentials(os.path.join('credentials','deepsound.txt'))
-    sendmail(html, credentials, to_list, 'Deepsound News', output_file)
+    #Obtain current day
+    with open('day.txt', 'r') as file:
+        lines = file.readlines()
+        day = lines[0][:-1]
     
-elif day=='Day 7':
-    """ #################
-    credentials= getcredentials(os.path.join('credentials','diana.txt'))
-    sendmail(html, credentials, to_list, 'Diana\'s SOTD', output_file)
-    """ #################
-    music_path = os.path.join(os.getcwd(),'music', 'Brian Eno The Big Ship.mp3')
-    video = ingestvideo(os.path.join('animation','Frames'))
-    
-    if __name__ == '__main__':
-            
-        sound = multiprocessing.Process(name='sound', target=os.system, args=('play \'{0}\''.format(music_path),))
-        images = multiprocessing.Process(name='images', target=showvideo, args=(video,))
+    html_file=open(os.path.join('mails',day+'.html'), 'r', encoding='utf-8')
+    html=html_file.read()
+    html_file.close()
 
-        sound.start()
-        images.start()
+    if day=='Day 0' or day=='Termination':
+        credentials = getcredentials(os.path.join('credentials','deepsound.txt'))
+        sendmail(html, credentials, to_list, 'Deepsound News', output_file)
+        
+    elif day=='Day 7':
+        
+        credentials= getcredentials(os.path.join('credentials','diana.txt'))
+        sendmail(html, credentials, to_list, 'Diana\'s SOTD', output_file)
+        
+        music_path = os.path.join(os.getcwd(),'music', 'Brian Eno The Big Ship.mp3')
+        video = ingestvideo(os.path.join('animation','Frames'))
+        
+        if __name__ == '__main__':
+                
+            sound = multiprocessing.Process(name='sound', target=os.system, args=('play \'{0}\''.format(music_path),))
+            images = multiprocessing.Process(name='images', target=showvideo, args=(video,))
 
-else:
-    credentials = getcredentials(os.path.join('credentials','diana.txt'))
-    sendmail(html, credentials, to_list, 'Diana\'s SOTD', output_file)
-    showimage(os.path.join('img',day+'.png'))
+            sound.start()
+            images.start()
 
-#Delete day from the file, so next time the program is executed, next day gets read
-with open("day.txt", "w") as file:
-    for pos, line in enumerate(lines):
-        if pos != 0:
-            file.write(line)
+    else:
+        credentials = getcredentials(os.path.join('credentials','diana.txt'))
+        sendmail(html, credentials, to_list, 'Diana\'s SOTD', output_file)
+        showimage(os.path.join('img',day+'.png'))
+
+    #Delete day from the file, so next time the program is executed, next day gets read
+    with open("day.txt", "w") as file:
+        for pos, line in enumerate(lines):
+            if pos != 0:
+                file.write(line)
+
+schedule.every(5).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@
 # Enabled i2c devices
 modprobe i2c-dev
 
-sleep 60
 # Start app
-#python3 src/main.py
-
-#play '/usr/src/app/src/music/Brian Eno The Big Ship.mp3'
+python3 src/main.py
