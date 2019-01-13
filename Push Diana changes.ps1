94bea90c6eaa323929c7f7d71c3a15aae6c438b5
luisverges <luisverges@gmail.com>
2019-01-13 16:53:47 +0100

    Testing

---
+++ Push Diana changes.ps1
@@ -0,0 +1,4 @@
+git add *
+git commit -m 'Testing'
+git push
+git push balena master
--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 # Enabled i2c devices
 modprobe i2c-dev
-
+ ls /
+ ls /usr
+ ls /usr/bin
 # Start app
 #python3 src/main.py
 mpg123 /usr/src/app/src/music/Brian Eno The Big Ship.mp3
