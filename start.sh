4fc15290d9537599c1c0dda1d1e61734d671bb3c
luisverges <luisverges@gmail.com>
2019-01-13 14:43:16 +0100

    Testing

--- start.sh
+++ start.sh
@@ -2,14 +2,10 @@
 
 # Enabled i2c devices
 modprobe i2c-dev
-ckconfig --list cron
-ckconfig --list crond
-ls /etc/
-chkconfig --list | grep crond
+
 # Start app
 #python3 src/main.py
-chkconfig crond on
-chkconfig cron on
+
 #echo new cron into cron file
 ps -ef | grep cron
 ps aux | grep crond
