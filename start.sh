aa775c45a9299578153b2e1f1e0bacc17be89f06
luisverges <luisverges@gmail.com>
2019-01-13 17:14:44 +0100

    Testing

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # Enabled i2c devices
 modprobe i2c-dev
 
-whereis ffplay
+
 whereis mocp
 # Start app
 #python3 src/main.py
-ffplay '/usr/src/app/src/music/Brian Eno The Big Ship.mp3'
+
 mocp '/usr/src/app/src/music/Brian Eno The Big Ship.mp3'
