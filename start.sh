0191222c848df0fd645aad5ca5dd52bdb787ba29
luisverges <luisverges@gmail.com>
2019-01-13 17:02:01 +0100

    Testing

--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 
 # Enabled i2c devices
 modprobe i2c-dev
- ls /
- ls /usr
- ls /usr/bin
+whereis mpg123
+locate mpg123
 # Start app
 #python3 src/main.py
 mpg123 /usr/src/app/src/music/Brian Eno The Big Ship.mp3
